ff5a7d818f787385fcaf131e059c6136292c786a
Frank South <frankstonesouth@gmail.com>
2022-04-01 17:56:46 -0400

    Added keyboard functionality

--- app.js
+++ app.js
@@ -26,3 +26,27 @@ buttons.map( button => {
         }
     })
 })
+
+window.addEventListener('keydown', (e) => {
+    switch(e.key){
+        case 'AC':
+            output.innerText = '';
+            break; 
+        case 'DEL':
+            if(output.innerText){
+                output.innerText = output.innerText.slice(0, -1);
+            break;
+            }
+        case 'Enter':
+            try{
+                output.innerText = eval(output.innerText);
+            }
+            catch {
+                output.innerText = 'Error!';
+            }
+            break;
+        default: 
+            output.innerText += e.key;
+    }
+})
+
